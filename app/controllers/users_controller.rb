7164b99afee3187893feb14914abcde271e04b1f
Zain-Razzaq <zainrazzaq2003@gmail.com>
2025-08-18 12:50:07 +0500

    Executed Rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_path, notice: 'User created successfully.'
+      redirect_to root_path, notice: "User created successfully."
     else
-      render :new, alert: 'Error creating user.'
+      render :new, alert: "Error creating user."
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root 'home#show'
+  root "home#show"
 
-  get 'login', to: 'sessions#new', as: 'login'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy', as: 'logout'
-  get 'signup', to: 'users#new', as: 'signup'
+  get "login", to: "sessions#new", as: "login"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy", as: "logout"
+  get "signup", to: "users#new", as: "signup"
   resources :sessions, only: [ :new, :create, :destroy ]
   resources :users, only: [ :new, :show, :create ]
 end
