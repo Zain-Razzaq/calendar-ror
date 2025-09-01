a14d3766cd6a3ae7ba57bfa08234b48f27d972e4
Zain-Razzaq <zainrazzaq2003@gmail.com>
2025-09-01 15:09:38 +0500

    Added env

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 require "stripe"
 
 Rails.application.config.stripe = {
-  publishable_key: "pk_test_51S2SqBLP6uE76iFa52BTtw42BRcEArS05scNRiRyX1s27N7RQCguSzABKshDpIwbfLD00X3YUyNO9af78nno9oMk006BcV3ABQ",
-  secret_key: "sk_test_51S2SqBLP6uE76iFa8sP23GXOaJDOHWXlsLqGcJteRr1pg1PBHAG9yP5pAq1ejPawxGIloyzTpueIdypWSX1JlmAX00IUXY4CuJ"
+  publishable_key: ENV["STRIPE_TEST_PUBISHABLE_KEY"],
+  secret_key: ENV["STRIPE_TEST_SECRET_KEY"]
 }
 
 Stripe.api_key = Rails.application.config.stripe[:secret_key]
